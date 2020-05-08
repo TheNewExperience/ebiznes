a93722c8341068265b0dc877f7fcfb18918e0118
TomaszSIola <tomasz.siola@xelto.com>
2020-05-08 15:16:06 +0200

    init react

--- app/models/Category.scala
+++
@@ -1,7 +0,0 @@
-package models
-import play.api.libs.json._
-case class Category(id_category: Int,category_name:String)
-
-object Category {
-  implicit val categoryFormat = Json.format[Category]
-}
